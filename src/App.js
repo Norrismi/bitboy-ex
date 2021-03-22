24976191bc72bedf3273a6db2e13fac848c92060
Norrismi <Norrismi0@gmail.com>
2021-03-21 22:14:14 -0400

    connecting to api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+// import React, {useState, useEffect} from 'react'
+// import axios from 'axios'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Challenge from './components/challenge/challenge'
 import Home from './components/home/home'
@@ -6,11 +7,34 @@ import TopNav from './components/Nav/TopNav/TopNav'
 
 
 function App() {
+
+// const [items, setItems] = useState([])
+// const [isLoading, setIsLoading] = useState(true)
+
+
+// useEffect(()=>{
+//   const fetchItems = async () => {
+
+//     //https://api.binance.us/api/v3/ticker/24hr
+//     const result = await axios(`https://api.binance.us/api/v3/ticker/24hr`)
+
+//     //console.log( result.data)
+//     setItems(result.data)
+//     setIsLoading(false)
+
+//   }
+//   fetchItems()
+// }, [])
+
+
+
+
+
   return (
 <Router>
   <TopNav/>
   <Switch>
-    <Route path='/challenge' component={Challenge}/>
+    <Route path='/challenge' component={Challenge} />
     <Route path='/' component={Home}/>
   </Switch>
 </Router>
--- src/components/challenge/challenge.js
+++ src/components/challenge/challenge.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-const challenge = () => {
-    return (
-        <div>
-            Hello from challenge
-        </div>
-    );
+const challenge = ({items, isLoading}) => {
+
+// console.log(props.items)
+console.log(items, isLoading)
+
+// let isLoading = props.isLoading
+
+return isLoading ? (<h1>Loading...</h1>) : (
+<div>
+Hello from items
+   {/* { items.map(item => (
+       <h1>{item.name}</h1>
+   ))} */}
+</div>
+)
+
+    // return (
+    //     <div>
+    //         Hello from challenge
+    //     </div>
+    // );
 }
 
 export default challenge;
